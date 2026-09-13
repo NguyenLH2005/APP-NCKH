6ea1e4700a316e6e323632f31f04c2fec500c3dc
FatCat <nguyenlh1012@gmail.com>
2026-09-13 15:51:28 +0700

    them cau hinh de tao file apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+
+            // TODO: Add your own ProGuard rules for the release build.
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,3 @@
+# Bỏ qua cảnh báo thiếu class của các ngôn ngữ ML Kit không sử dụng
+-dontwarn com.google.mlkit.vision.text.**
+-keep class com.google.mlkit.vision.text.** { *; }
